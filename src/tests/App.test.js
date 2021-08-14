6c2599694a32498356b7c841131b1f0b9e7db35d
EvgenSavko <senyaq@gmail.com>
2021-08-14 23:15:09 +0300

    feat: creat one test for Login

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,36 @@
 import { render, screen } from '@testing-library/react'
+import React, { createContext } from 'react'
+
 import App from '../App'
 import Btn from '@Components/Button/Button'
+
+const AuthContext = createContext()
+
+const AuthProvider2 = ({ children }) => {
+  const value = {
+    currentUser: {
+      email: 'test',
+      uid: '123',
+      emailVerified: true,
+    },
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+jest.mock('@Shared/context/AuthContext', () => {
+  const useAuth = () => {
+    return {
+      currentUser: {
+        email: 'test',
+        uid: '123',
+        emailVerified: true,
+      },
+    }
+  }
+
+  return { AuthProvider: AuthProvider2, useAuth }
+})
 
 test('renders header title', () => {
   render(<App />)
